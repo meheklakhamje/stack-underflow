d0c8d31e3f9cb5ecc9e5d684bae83f34b57847df
meheklakhamje <lakhamjemehek@gmail.com>
2026-09-12 01:45:19 +0530

    login view created

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
@@ -25,3 +26,27 @@ def register(request):
         form = RegisterForm()
 
     return render(request, "register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+        return render(
+            request,
+            "login.html",
+            {"error": "Invalid username or password."},
+        )
+
+    return render(request, "login.html")
--- stackunderflow/urls.py
+++ stackunderflow/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home, register
+from core.views import home, register, login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path("register/", register, name="register"),
+    path("login/", login_view, name="login"),
 ]
